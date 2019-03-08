cc8ab967847bc3f2fa0db5f4919fff1982de002c
Laurent Bassin <laurent@bassin.info>
2019-03-08 11:55:03 +0100

    dr'tfgyzeqrhiklbd ,mfg

--- api/src/Controller/Ticket/Pdf.php
+++ api/src/Controller/Ticket/Pdf.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Controller\Ticket;
 
 use App\Entity\Ticket;
-use Dompdf\Dompdf;
-use Dompdf\Options;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Twig\Environment as Twig;
 
 class Pdf
@@ -20,15 +20,23 @@ class Pdf
 
     public function __invoke(Ticket $data)
     {
-        $dompdf = new Dompdf(new Options(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]));
-        $dompdf->setPaper('A4', 'portrait');
+        $html = $this->twig->render('ticket/pdf.html.twig', [
+            'ticket' => $data,
+        ]);
 
-        $html = $this->twig->render('ticket/pdf.html.twig');
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML($html);
 
-        $dompdf->loadHtml($html);
+        $response = new Response($mpdf->Output('', 'S'));
 
-        $dompdf->render();
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            'ticket.pdf'
+        );
 
-        $dompdf->stream('ticket.pdf');
+        $response->headers->set('Content-Disposition', $disposition);
+        $response->headers->set('Content-Type', 'application/pdf');
+
+        return $response;
     }
 }
